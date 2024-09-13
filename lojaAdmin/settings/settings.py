07b5f5fc5a55b31a3088e723eb2bc2ec8ffbac4c
Iasmim Paula Souto <123132291+iasps@users.noreply.github.com>
2024-09-13 13:36:19 +0000

    Atualização

--- lojaAdmin/settings/settings.py
+++ lojaAdmin/settings/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = ['https://localhost:8080', 'http://127.0.0.1:8080']
+CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'http://127.0.0.1:8000']
 
 
 
